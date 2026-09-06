778d774abd7eb30631417a1e6c0208784ef12ec0
LeskGit <hacksel002@gmail.com>
2026-09-06 14:54:32 +0200

    Semaine 01 : 06/09/2026

---
+++ PASS Java/Conditionnelles répetitives/Main/src/App.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class App {
+
+
+    public static void main(String[] args) throws Exception {
+
+        /* If else classic */
+        
+        Scanner scanner = new Scanner(System.in);
+        // double input = scanner.nextDouble();
+        // String appreciation;
+
+        // if (input < 10) {
+        //     appreciation = "Ajourné";
+        // }
+        // else if (input < 12) {
+        //     appreciation = "Passable";
+        // }
+        // else if (input < 14) {
+        //     appreciation = "Assez bien";
+        // }
+        // else if (input < 16) {
+        //     appreciation = "Bien";
+        // }
+        // else {
+        //     appreciation = "Très bien";
+        // }
+
+        // System.out.println(appreciation);
+
+        /* Switch Case */
+    
+        // int input = scanner.nextInt();
+        // String day = "";
+
+        // switch (input) {
+        //     case 1: day = "Lundi"; break;
+        //     case 2: day = "Mardi"; break;
+        //     case 3: day = "Mercredi"; break;
+        //     case 4: day = "Jeudi"; break;
+        //     case 5: day = "Vendredi"; break;
+        //     case 6: day = "Samedi"; break;
+        //     case 7: day = "Dimanche"; break;
+        //     default: day = "Invalide"; break;
+        // }
+
+        // System.out.println(day);
+
+        /* Boucle for */
+
+        // int input = scanner.nextInt();
+
+        // for (int i = 1; i <= 10; i++) {
+        //     System.out.println(input + " * " + i + " = " + (input * i));
+        // }
+
+        /* While */
+
+        // System.out.println("Nombre secret : ");
+        // int secret = scanner.nextInt();
+        // System.out.println("Nombre divisé : ");
+        // int nombre = scanner.nextInt();
+
+        // while (nombre > secret) {
+        //     System.out.println(nombre);
+        //     nombre = nombre / 2;
+        // }
+
+        // do {
+        //     System.out.println(nombre);
+        //     nombre = nombre / 2;
+        // } while (nombre > secret);
+
+        // System.out.println(nombre);
+
+        /* Si nombre inferieur directement, le do while execute quand meme une fois => do while execute puis tests, while test puis execute */
+        
+
+    }
+}
